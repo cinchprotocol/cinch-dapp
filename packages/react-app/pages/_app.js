3c7b5542c87dbf8be368123b1e4236b1626c0033
Sing <16409791+singyiu@users.noreply.github.com>
2022-07-25 11:48:33 -0700

    empty landing page

--- packages/react-app/pages/_app.js
+++ packages/react-app/pages/_app.js
@@ -5,9 +5,11 @@ import { Web3Provider } from "../helpers/Web3Context";
 import { DevUI, NetworkDisplay, ThemeSwitch } from "../components";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
   const prevTheme = useRef("light");
+  const router = useRouter();
 
   const themes = {
     dark: `/css/dark-theme.css`,
@@ -28,9 +30,9 @@ function MyApp({ Component, pageProps }) {
               href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏗</text></svg>"
             />
           </Head>
-          <NetworkDisplay />
-          <DevUI />
-          <ThemeSwitch />
+          {router?.pathname === "/" ? null : <NetworkDisplay />}
+          {router?.pathname === "/" ? null : <DevUI />}
+          {router?.pathname === "/" ? null : <ThemeSwitch />}
           <Component {...pageProps} />
         </>
       </ThemeSwitcherProvider>

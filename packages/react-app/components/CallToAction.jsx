62359d6e8c2ac8868e91078e480182acb5a7f9c3
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-07-31 17:41:04 -0700

    updated CTA

--- packages/react-app/components/CallToAction.jsx
+++ packages/react-app/components/CallToAction.jsx
@@ -6,24 +6,21 @@ import backgroundImage from '/images/background-call-to-action.jpg'
 
 export function CallToAction() {
   return (
-    <section
-      id="get-started-today"
-      className="relative overflow-hidden bg-gradient-to-r from-green-400 to-blue-500 py-32"
-    >
-      
-      <Container className="relative">
-        <div className="mx-auto max-w-lg text-center">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Have any question?
-          </h2>
-          <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your currency.
-          </p>
-          <Button href="/register" color="white" className="mt-10">
-            Get in touch
-          </Button>
-        </div>
-      </Container>
-    </section>
+    <div className="bg-indigo-700">
+    <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+        <span className="block">Ready to dive in?</span>       
+      </h2>
+      <p className="mt-4 text-lg leading-6 text-indigo-200">
+        Want to see how Cinch's solution can transform how you manage your treasury? reach out to us below.
+      </p>
+      <a
+        href="#"
+        className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 sm:w-auto"
+      >
+        Get in touch
+      </a>
+    </div>
+  </div>
   )
 }

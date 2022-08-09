9dfeb2b90029217a125321195058775b8e326cb4
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-08-08 20:55:17 -0700

    Added cinch logo

--- packages/react-app/components/Logo.jsx
+++ packages/react-app/components/Logo.jsx
@@ -1,16 +1,16 @@
 export function Logo(props) {
   return (
     <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-blue-600.svg" alt="Workflow"></img>
-            </div>
-            <div class="hidden md:block">
-              <span class="ml-3 text-600 text-2xl">
-                Cinch
-              </span>
-            </div>
-          </div>         
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+          <img class="h-10 w-10" src="/cinch_logo.png" alt="Cinch"></img>
         </div>
+        <div class="hidden md:block">
+          <span class="ml-2 text-600 text-3xl text-blue-600 font-semibold">
+            Cinch
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }

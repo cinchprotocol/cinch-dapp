bdc2dd42fa9cbf0ec1a9ea5ad67d1673c6538eea
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-08-08 22:01:16 -0700

    Logo size update

--- packages/react-app/components/Logo.jsx
+++ packages/react-app/components/Logo.jsx
@@ -3,10 +3,10 @@ export function Logo(props) {
     <div class="flex items-center justify-between">
       <div class="flex items-center">
         <div class="flex-shrink-0">
-          <img class="h-10 w-10" src="/cinch_logo.png" alt="Cinch"></img>
+          <img class="h-12 w-12" src="/cinch_logo.png" alt="Cinch"></img>
         </div>
         <div class="hidden md:block">
-          <span class="ml-1 text-600 text-3xl text-blue-600 font-semibold">
+          <span class="ml-1 text-600 text-4xl text-blue-600 font-semibold">
             Cinch
           </span>
         </div>

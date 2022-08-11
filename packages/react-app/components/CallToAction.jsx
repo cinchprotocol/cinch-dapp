c9cafe6bbab2140c9665a578cd4c9e5f70863d6f
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-08-11 15:23:41 -0700

    updated CTA colors

--- packages/react-app/components/CallToAction.jsx
+++ packages/react-app/components/CallToAction.jsx
@@ -6,12 +6,12 @@ import backgroundImage from "/images/background-call-to-action.jpg";
 
 export function CallToAction() {
   return (
-    <div className="bg-indigo-700">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-600">
       <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
           <span className="block">Ready to dive in?</span>
         </h2>
-        <p className="mt-4 text-lg leading-6 text-indigo-200">
+        <p className="mt-4 text-lg leading-6 text-blue-200">
           Connect with us to find out how revenue-share tokens can transform how you manage your treasury.
         </p>
         <a

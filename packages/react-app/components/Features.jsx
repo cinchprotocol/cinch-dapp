321a200c7bb32ddde3bbb6d63ebedbf02c10d83f
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-08-17 15:17:29 -0700

    Updated feature section

--- packages/react-app/components/Features.jsx
+++ packages/react-app/components/Features.jsx
@@ -43,52 +43,35 @@ const features = [
 
 export function Features() {
   return (
-    <div className="bg-gray-50 overflow-hidden">
-      <div className="relative max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <svg
-          className="absolute top-0 left-full transform -translate-x-1/2 -translate-y-3/4 lg:left-auto lg:right-full lg:translate-x-2/3 lg:translate-y-1/4"
-          width={404}
-          height={784}
-          fill="none"
-          viewBox="0 0 404 784"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="8b1b5f72-e944-4457-af67-0c6d15a99f38"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={784} fill="url(#8b1b5f72-e944-4457-af67-0c6d15a99f38)" />
-        </svg>
+    <div className="py-12 bg-cyan-50">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="lg:text-center">
+        <h2 className="text-lg text-blue-600 font-semibold">Features</h2>
+        <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl sm:tracking-tight">
+          Tools for DAOs
+        </p>
+        {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
+          accusamus quisquam.
+        </p> */}
+      </div>
 
-        <div className="relative lg:grid lg:grid-cols-3 lg:gap-x-8">
-          <div className="lg:col-span-1">
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Tools for DAOs
-            </h2>
-          </div>
-          <dl className="mt-10 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-10 lg:mt-0 lg:col-span-2">
-            {features.map((feature) => (
-              <div key={feature.name}>
-                <dt>
-                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-600 text-white">
-                    <feature.icon className="h-6 w-6" aria-hidden="true" />
-                  </div>
-                  <p className="mt-5 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
-                </dt>
-                <dd className="mt-2 text-base text-gray-500">{feature.description}</dd>
-              </div>
-            ))}
-          </dl>
-        </div>
+      <div className="mt-10">
+        <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative">
+              <dt>
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
+                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
       </div>
     </div>
+  </div>
   )
 }

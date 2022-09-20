1423a3baa2373477158030368205035d1df47e94
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-09-20 12:28:15 -0700

    Updated rev input form

--- packages/react-app/pages/revenueroyaltyinputs.js
+++ packages/react-app/pages/revenueroyaltyinputs.js
@@ -43,141 +43,152 @@ function RevenueRoyaltyInputs({ web3 }) {
     <>
       <CommonHead />
       <DAppHeader web3={web3} />
+      <header>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+
+        </div>
+      </header>
       <main>
-        <div className="flex flex-1 flex-col h-screen w-full items-center">
-          <div className="" style={{ margin: 32 }}>
-            <Form
-              name="basic"
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 
-              wrapperCol={{
-                span: 24,
-              }}
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFormFinish}
-              onFinishFailed={onFormFinishFailed}
-              autoComplete="off"
-              requiredMark={false}
-              layout="verticle"
-              size="large"
-            >
-              <Form.Item
-                label="Fee_collector contract address"
-                name="feeCollectorContractAddress"
-                extra="Enter the contract address that consolidates fees(The revenue royalty will be implemented by changing
+          <div class="px-4 py-8 sm:px-0">
+            <div class="p-10 rounded-lg bg-slate-50 shadow">
+              <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Enter details to implement revenue-share</h1>
+              <div className="max-w-lg">
+                <Form
+                  name="basic"
+                  wrapperCol={{
+                    span: 24,
+                  }}
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={onFormFinish}
+                  onFinishFailed={onFormFinishFailed}
+                  autoComplete="off"
+                  requiredMark={false}
+                  layout="verticle"
+                  size="large"
+                  requiredMark="required"
+                >
+                  <Form.Item
+                    label="Fee_collector contract address"
+                    name="feeCollectorContractAddress"
+                    extra="Enter the contract address that consolidates fees(The revenue royalty will be implemented by changing
                   the destination wallet address of this contract)"
-                rules={[
-                  {
-                    required: true,
-                    message:
-                      "Enter the contract address that consolidates fees(The revenue royalty will be implemented by changing the destination wallet address of this contract)",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+                    rules={[
+                      {
+                        required: true,
+                        message:
+                          "Enter the contract address that consolidates fees(The revenue royalty will be implemented by changing the destination wallet address of this contract)",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
 
 
-              <Form.Item
-                label="Multi-sig_address"
-                name="multiSigAddress"
-                extra="Contrat that controls the inputs to the fee_collector contract"
-                rules={[
-                  {
-                    required: true,
-                    message: "Contrat that controls the inputs to the fee_collector contract",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+                  <Form.Item
+                    label="Multi-sig_address"
+                    name="multiSigAddress"
+                    extra="Contrat that controls the inputs to the fee_collector contract"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Contrat that controls the inputs to the fee_collector contract",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
 
-              <Form.Item
-                label="Revenue_proportion"
-                name="revenueProportion"
-                extra="% of revenue traded as a royalty"
-                rules={[
-                  {
-                    required: true,
-                    message: "% of revenue traded as a royalty",
-                  },
-                ]}
-              >
-                <InputNumber
-                  style={{
-                    width: "100%",
-                  }}
-                  placeholder="%"
-                />
-              </Form.Item>
-
-
-              <Form.Item
-                label="Expiry_amount"
-                name="expiryAmount"
-                extra="Royalty will end after this amount of revenue"
-                rules={[
-                  {
-                    required: true,
-                    message: "Royalty will end after this amount of revenue",
-                  },
-                ]}
-              >
-                <InputNumber
-                  style={{
-                    width: "100%",
-                  }}
-                  placeholder="Amount"
-                />
-              </Form.Item>
+                  <Form.Item
+                    label="Revenue_proportion"
+                    name="revenueProportion"
+                    extra="% of revenue traded as a royalty"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Enter valid % number",
+                      },
+                    ]}
+                  >
+                    <InputNumber
+                      style={{
+                        width: "100%",
+                      }}
+                      placeholder="%"
+                    />
+                  </Form.Item>
 
 
-              <Form.Item
-                label="Contact information (optional)"
-                name="contactInformation"
-                extra="Enter your preferred contact information to receive notifications on the status of your royalty listing,
+                  <Form.Item
+                    label="Expiry_amount"
+                    name="expiryAmount"
+                    extra="Royalty will end after this amount of revenue"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Royalty will end after this amount of revenue",
+                      },
+                    ]}
+                  >
+                    <InputNumber
+                      style={{
+                        width: "100%",
+                      }}
+                      placeholder="Amount"
+                    />
+                  </Form.Item>
+
+
+                  <Form.Item
+                    label="Contact information (optional)"
+                    name="contactInformation"
+                    extra="Enter your preferred contact information to receive notifications on the status of your royalty listing,
                 its implementation, and its performance"
-                rules={[
-                  {
-                    required: false,
-                    message:
-                      "Enter your preferred contact information to receive notifications on the status of your royalty listing, its implementation, and its performance",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+                    rules={[
+                      {
+                        required: false,
+                        message:
+                          "Enter your preferred contact information to receive notifications on the status of your royalty listing, its implementation, and its performance",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
 
 
-              <Form.Item
-                wrapperCol={{
-                  offset: 8,
-                  span: 16,
-                }}
+                  <Form.Item
+
+                  >
+                    <Button type="primary" htmlType="submit">
+                      Review details
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
+
+              <Modal
+                title="Revenue royalty listing"
+                visible={isModalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                okText="Confirm list revenue royalty"
               >
-                <Button type="primary" htmlType="submit">
-                  View listing
-                </Button>
-              </Form.Item>
-            </Form>
+                <p>Summary</p>
+                <p>feeCollectorContractAddress {formValues?.feeCollectorContractAddress}</p>
+                <p>multiSigAddress {formValues?.multiSigAddress}</p>
+                <p>revenueProportion {formValues?.revenueProportion}</p>
+                <p>expiryAmount {formValues?.expiryAmount}</p>
+                <p>contactInformation {formValues?.contactInformation}</p>
+              </Modal>
+            </div>
           </div>
-          <Modal
-            title="Revenue royalty listing"
-            visible={isModalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            okText="Confirm list revenue royalty"
-          >
-            <p>Summary</p>
-            <p>feeCollectorContractAddress {formValues?.feeCollectorContractAddress}</p>
-            <p>multiSigAddress {formValues?.multiSigAddress}</p>
-            <p>revenueProportion {formValues?.revenueProportion}</p>
-            <p>expiryAmount {formValues?.expiryAmount}</p>
-            <p>contactInformation {formValues?.contactInformation}</p>
-          </Modal>
+
         </div>
       </main>
+
       <Footer />
     </>
   );

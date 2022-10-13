3702845938addae33536eadfac867b2a6811bcdb
Sing <16409791+singyiu@users.noreply.github.com>
2022-10-12 20:10:57 -0700

    handleAcceptBid

--- packages/react-app/components/BidTable.jsx
+++ packages/react-app/components/BidTable.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Table, Space } from "antd";
+import { Table, Space, message } from "antd";
 import _ from "lodash";
 import { Button } from "/components/Button";
+import { useRouter } from "next/router";
 
 function BidTable({ web3, dataSource, mode }) {
+  const router = useRouter();
+  const marketPlaceContract = web3?.writeContracts["MarketPlace"];
+
   const handleCancelBid = async record => {
     if (record && record.itemId) {
-      const marketPlaceContract = web3?.writeContracts["MarketPlace"];
-      const txRes = await web3?.tx(marketPlaceContract?.cancelBid(record.itemId), res => {
-        console.log("📡 Transaction cancelBid:", res);
-      });
+      const tx = await marketPlaceContract?.cancelBid(record.itemId);
+      const txRes = await tx?.wait();
       console.log("txRes", txRes);
+    }
+  };
+
+  const handleAcceptBid = async record => {
+    if (record && record.itemId) {
+      const tx = await marketPlaceContract?.acceptBid(record.itemId, record.id);
+      const txRes = await tx?.wait();
+      console.log("txRes", txRes);
+
+      if (txRes?.events?.find(e => e?.event === "BidAccepted")) {
+        message.success("Bid accepted");
+      } else if (txRes?.events?.find(e => e?.event === "BidRejected")) {
+        message.error("Bid rejected");
+      }
+
+      const vaultCreatedEvent = txRes?.events?.find(e => e?.event === "RBFVaultCreated");
+      if (vaultCreatedEvent && vaultCreatedEvent.args?.vaultAddress) {
+        router.push(`/vault/${vaultCreatedEvent?.args?.vaultAddress}`);
+      } else {
+        router.push("/dashboard");
+      }
     }
   };
 
@@ -46,7 +69,13 @@ function BidTable({ web3, dataSource, mode }) {
         if (isRevenueStreamSeller) {
           return (
             <Space size="middle">
-              <Button href={`/revenuestreams/${itemId}/acceptbids/${id}`}>Accept</Button>
+              <Button
+                onClick={() => {
+                  handleAcceptBid(record);
+                }}
+              >
+                Accept
+              </Button>
             </Space>
           );
         } else if (isBidder) {

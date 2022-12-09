5926dc092538720324cd460b6d7452a2f0a590df
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-12-08 19:45:45 -0800

    Renamed RBF vault

---
+++ packages/hardhat/contracts/Vault.sol
@@ -0,0 +1,247 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./interfaces/IGnosisSafe.sol";
+import "./MarketPlaceStorage.sol";
+
+interface IBorrowerContract {
+    function feeReceiver() external view returns (address);
+
+    function deposit(address tokenAddress, uint256 amount) external;
+
+    function withdraw(address tokenAddress, uint256 amount) external;
+
+    function owner() external view returns (address);
+}
+
+//TODO: Update the lender and borrower terms/concept used in this contract. In the Idle case, it would be staking and unstaking. In general, it would be buying and selling. In either case, we should clarify the docs.
+/**
+ * @title RBFVault
+ * @notice Contract allowing Lender to secure royalty revenue streams
+ * @dev Should be deployed per revenue stream.
+ */
+contract RBFVault is ERC4626Upgradeable {
+    event RBFVaultActivated();
+    event RBFVaultRefundInitiated();
+
+    enum Status {
+        Pending,
+        Active,
+        Expired,
+        Canceled
+    }
+
+    struct ProtocolDetail {
+        address feeCollector;
+        address multiSig;
+        uint256 expAmount;
+    }
+    ProtocolDetail public protocolDetail;
+    address public multisigGuard;
+
+    mapping(address => uint256) internal _totalValueLocked; // token => totalReleased
+
+    // If vault is not active after this timeout period then lender can withdraw the fund
+    uint256 public constant TIMEOUT_PERIOD = 1 weeks;
+
+    Status public vaultStatus;
+    uint256 public vaultActivationDate;
+    uint256 public vaultDeployDate;
+
+    uint256 internal storedTotalAssets;
+
+    /// @notice vault initializer
+    /// @param asset vault asset
+    /// @param name ERC20 name of the vault shares token
+    /// @param symbol ERC20 symbol of the vault shares token
+    function initialize(
+        address asset,
+        string memory name,
+        string memory symbol,
+        ProtocolDetail memory _protocolDetail,
+        address _multisigGuard
+    ) public initializer {
+        __ERC4626_init(IERC20Upgradeable(asset));
+        __ERC20_init(name, symbol);
+
+        protocolDetail.feeCollector = _protocolDetail.feeCollector;
+        protocolDetail.multiSig = _protocolDetail.multiSig;
+        protocolDetail.expAmount = _protocolDetail.expAmount;
+        multisigGuard = _multisigGuard;
+
+        vaultStatus = Status.Pending;
+        vaultDeployDate = block.timestamp;
+    }
+
+    //TODO: should this function be bounded to be called by the borrower only?
+    /**
+     * @dev Activates the vault after the required condition has been met and transfer funds to the borrower.
+     */
+    function activate() external {
+        _isValidState(Status.Pending);
+
+        require(isReadyToActivate(), "VAULT_ACTIVATION_TERMS_NOT_MET");
+
+        vaultStatus = Status.Active;
+        vaultActivationDate = block.timestamp;
+
+        emit RBFVaultActivated();
+    }
+
+    function deposit(uint256 assets, address receiver)
+        public
+        virtual
+        override
+        returns (uint256)
+    {
+        _isValidState(Status.Active);
+
+        uint256 shares = super.deposit(assets, receiver);
+        afterDeposit(assets, 0);
+        return shares;
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual override returns (uint256) {
+        _isValidState(Status.Active);
+
+        beforeWithdraw(assets, 0);
+        uint256 shares = super.withdraw(assets, receiver, owner);
+        return shares;
+    }
+
+    function depositWithReferral(
+        uint256 assets,
+        address receiver,
+        address referral
+    ) public returns (uint256) {
+        _isValidState(Status.Active);
+
+        uint256 shares = super.deposit(assets, receiver);
+        afterDeposit(assets, 0);
+        _totalValueLocked[referral] += assets;
+        return shares;
+    }
+
+    function withdrawWithReferral(
+        uint256 assets,
+        address receiver,
+        address owner,
+        address referral
+    ) public returns (uint256) {
+        _isValidState(Status.Active);
+
+        beforeWithdraw(assets, 0);
+        uint256 shares = super.withdraw(assets, receiver, owner);
+        _totalValueLocked[referral] -= assets;
+        return shares;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                         INTERNAL HOOKS LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function beforeWithdraw(uint256 amount, uint256) internal {
+        storedTotalAssets -= amount;
+
+        IBorrowerContract(protocolDetail.feeCollector).withdraw(
+            asset(),
+            amount
+        );
+    }
+
+    function afterDeposit(uint256 amount, uint256) internal {
+        IERC20(asset()).approve(protocolDetail.feeCollector, amount);
+        IBorrowerContract(protocolDetail.feeCollector).deposit(asset(), amount);
+
+        storedTotalAssets += amount;
+    }
+
+    /// @notice Total amount of the underlying asset that
+    /// is "managed" by Vault.
+    function totalAssets() public view override returns (uint256) {
+        return storedTotalAssets;
+    }
+
+    /************************/
+    /*** Helper Functions ***/
+    /************************/
+
+    /**
+     * @dev Check if the vault is ready to be activated
+     */
+    function isReadyToActivate() public view returns (bool) {
+        require(isFeeCollectorUpdated(), "FEE_COLLECTOR_RECEIVER_NOT_UPDATED");
+
+        require(isMultisigGuardAdded(), "MULTISIG_GUARD_NOT_IN_PLACE");
+
+        require(
+            isMultisigOwnsTheRevenueContract(),
+            "REVENUE_CONTRACT_NOT_OWNED_BY_PROVIDED_MULTISIG"
+        );
+
+        return true;
+    }
+
+    /**
+     * @dev Check if the fee collector is updated
+     */
+    function isFeeCollectorUpdated() public view returns (bool) {
+        return
+            IBorrowerContract(protocolDetail.feeCollector).feeReceiver() ==
+            address(this);
+    }
+
+    /**
+     * @dev Check if the provided multi-sig address is the revenue contract owner
+     */
+    function isMultisigOwnsTheRevenueContract() public view returns (bool) {
+        return
+            IBorrowerContract(protocolDetail.feeCollector).owner() ==
+            protocolDetail.multiSig;
+    }
+
+    /**
+     * @dev Check if cinch multi-sig guard is added
+     */
+    function isMultisigGuardAdded() public view returns (bool) {
+        return GnosisSafe(protocolDetail.multiSig).getGuard() == multisigGuard;
+    }
+
+    // /**
+    //  * @dev getVaultBalance
+    //  */
+    // function getVaultBalance() external view returns (uint256) {
+    //     return IERC20(asset).balanceOf(address(this));
+    // }
+
+    /**
+        @dev   Checks that the current state of the vault matches the provided state.
+        @param _status Enum of desired vault status.
+    */
+    function _isValidState(Status _status) internal view {
+        require(vaultStatus == _status, "INVALID_STATE");
+    }
+
+    /**
+     * @dev Getter of _totalValueLocked
+     */
+    function getTotalValueLocked(address referral)
+        external
+        view
+        returns (uint256)
+    {
+        return _totalValueLocked[referral];
+    }
+}
---
+++ packages/hardhat/test/Vault.test.js
@@ -0,0 +1,156 @@
+/* eslint-disable no-unused-expressions */
+const { ethers, upgrades } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+use(solidity);
+
+const sellerAccountIndex = 1;
+const buyerAccountIndex = 2;
+
+let accounts;
+let mockFeeCollector;
+let mockGnosisSafe;
+let cinchSafeGuard;
+let rbfVault;
+let mockERC20;
+let mockERC20Decimals;
+
+before(async function () {
+  // get accounts from hardhat
+  accounts = await ethers.getSigners();
+});
+
+describe("RBFVault tests", function () {
+  describe("Deploy", function () {
+    it("Should deploy MockFeeCollector", async function () {
+      const MockFeeCollector = await ethers.getContractFactory(
+        "SampleProtocol"
+      );
+
+      mockFeeCollector = await MockFeeCollector.deploy();
+      expect(mockFeeCollector.address).to.not.be.undefined;
+      console.log("mockFeeCollector.address: ", mockFeeCollector.address);
+    });
+    it("Should deploy MockGnosisSafe", async function () {
+      const MockGnosisSafe = await ethers.getContractFactory("MockGnosisSafe");
+
+      mockGnosisSafe = await MockGnosisSafe.deploy();
+      expect(mockGnosisSafe.address).to.not.be.undefined;
+      console.log("mockGnosisSafe.address: ", mockGnosisSafe.address);
+    });
+    it("Should deploy MockERC20", async function () {
+      const MockERC20 = await ethers.getContractFactory("TestToken");
+      mockERC20 = await MockERC20.deploy();
+      mockERC20Decimals = await mockERC20.decimals();
+      expect(mockERC20.address).to.not.be.undefined;
+    });
+    it("Should deploy CinchSafeGuard", async function () {
+      const CinchSafeGuard = await ethers.getContractFactory("CinchSafeGuard");
+
+      cinchSafeGuard = await CinchSafeGuard.deploy();
+      expect(cinchSafeGuard.address).to.not.be.undefined;
+      console.log("cinchSafeGuard.address: ", cinchSafeGuard.address);
+    });
+    it("Should deploy and Initialize RBFVault", async function () {
+      const RBFVault = await ethers.getContractFactory("RBFVault", accounts[0]);
+
+      const protocolDetail = {
+        feeCollector: mockFeeCollector.address,
+        multiSig: mockGnosisSafe.address,
+        expAmount: 1000000,
+      };     
+
+      rbfVault = await upgrades.deployProxy(RBFVault, [
+        mockERC20.address,
+        "CinchPx",
+        "CPX",
+        protocolDetail,
+        cinchSafeGuard.address
+      ]);
+
+      expect(rbfVault.address).to.not.be.undefined;
+      console.log("rbfVault.address: ", rbfVault.address);
+    });
+
+  });
+  describe("activate", function () {
+    it("should revert if FEE_COLLECTOR_RECEIVER_NOT_UPDATED", async function () {
+      const tx = rbfVault.activate();
+      await expect(tx).to.be.revertedWith("FEE_COLLECTOR_RECEIVER_NOT_UPDATED");
+    });
+
+    it("feeReceiver should be updated", async function () {
+      const tx = await mockFeeCollector.setFeeReceiver(rbfVault.address);
+      expect(tx).to.emit(mockFeeCollector, "FeeReceiverUpdated");
+    });
+
+
+    it("should revert if MULTISIG_GUARD_NOT_IN_PLACE", async function () {
+      const tx = rbfVault.activate();
+      await expect(tx).to.be.revertedWith("MULTISIG_GUARD_NOT_IN_PLACE");
+    });
+
+    it("setGuard should be processed", async function () {
+      const tx = await mockGnosisSafe.setGuard(cinchSafeGuard.address);
+      expect(tx).to.emit(mockGnosisSafe, "GuardUpdated");
+    });
+
+    it("should revert if REVENUE_CONTRACT_NOT_OWNED_BY_PROVIDED_MULTISIG", async function () {
+      const tx = rbfVault.activate();
+      await expect(tx).to.be.revertedWith(
+        "REVENUE_CONTRACT_NOT_OWNED_BY_PROVIDED_MULTISIG"
+      );
+    });
+    it("transferOwnership should work", async function () {
+      const tx = await mockFeeCollector
+        .connect(accounts[0])
+        .transferOwnership(mockGnosisSafe.address);
+      expect(tx).to.emit(mockFeeCollector, "OwnershipTransferred");
+    });
+
+    it("should be activated", async function () {
+      const tx01 = mockERC20
+        .connect(accounts[0])
+        .faucet(rbfVault.address, 1000 * 10 ** 12);
+      await expect(tx01).not.to.be.revertedWith();
+      const tx03 = await rbfVault.activate();
+      expect(tx03).to.emit(rbfVault, "RBFVaultActivated");
+    });
+
+    it("should not be activated twice", async function () {
+      const tx = rbfVault.activate();
+      await expect(tx).to.be.revertedWith("INVALID_STATE");
+    });
+  });
+
+  describe("Transactions", function () {
+
+    it("can't deposit before approval", async function () {
+      const tx = rbfVault.connect(accounts[1]).deposit(500 * mockERC20Decimals, accounts[1].address);
+      await expect(tx).to.be.revertedWith("ERC20: insufficient allowance");
+    });
+
+    it("should fail if sender doesn't have enough funds", async function () {
+      await mockERC20.faucet(accounts[1].address, 1000 * mockERC20Decimals);
+      await mockERC20.connect(accounts[1]).approve(rbfVault.address, 1000 * mockERC20Decimals);
+      const tx = rbfVault.connect(accounts[1]).deposit(1001 * mockERC20Decimals, accounts[1].address);
+      await expect(tx).to.be.revertedWith("ERC20: insufficient allowance");
+    });
+
+    it("should be able to deposit", async function () {
+      const tx = rbfVault.connect(accounts[1]).deposit(1000 * 1000000, accounts[1].address);
+      //await expect(tx).to.equal(1000*(10**6));
+      expect(await rbfVault.balanceOf(accounts[1].address)).to.equal(1000 * (10 ** 6));
+    });
+
+    // it("should be able to withdraw", async function () {
+    //   const tx = rbfVault.connect(accounts[1]).withdraw(1000 * 1000000, accounts[1].address, accounts[1].address);
+    //   expect(await rbfVault.balanceOf(accounts[1].address)).to.equal(0);
+    // });
+
+  });
+
+
+
+});

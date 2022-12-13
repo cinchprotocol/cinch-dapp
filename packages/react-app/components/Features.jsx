657a2197555daad7a8f4df0872886b9294a18666
Sing <16409791+singyiu@users.noreply.github.com>
2022-12-12 19:24:52 -0800

    landing page updates

--- packages/react-app/components/Features.jsx
+++ packages/react-app/components/Features.jsx
@@ -50,9 +50,9 @@ export function Features() {
     <div className="py-12 bg-sky-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" data-aos="fade-up">
         <div className="text-center">
-          <h2 className="text-lg text-blue-600 font-semibold">One stop go-to-market solution</h2>
+          <h2 className="text-lg text-blue-600 font-semibold">One stop go-to-market solution for protocols</h2>
           <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl sm:tracking-tight">
-            Cinch Protocol puts amazing DeFi products in front of everyday people.
+            Cinch puts amazing DeFi products in front of everyday people.
           </p>
           {/* <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
           Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in

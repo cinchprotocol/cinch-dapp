d61988e5bdbbf346a209500598dbeedce743b8c6
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-12-15 16:23:49 -0800

    removed unused import

--- packages/hardhat/contracts/Vault.sol
+++ packages/hardhat/contracts/Vault.sol
@@ -3,15 +3,12 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-//import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 
 import "./interfaces/IGnosisSafe.sol";
-import "./MarketPlaceStorage.sol";
 
 interface IYieldSourceContract {
     function feeReceiver() external view returns (address);

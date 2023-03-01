05b68909754f8986654bf9ba8e7e98a9c577a1e0
Sing <16409791+singyiu@users.noreply.github.com>
2023-02-28 16:34:32 -0800

    getYieldSourceVaultTotalShares

--- packages/hardhat/contracts/Vault.sol
+++ packages/hardhat/contracts/Vault.sol
@@ -344,7 +344,7 @@ contract Vault is ERC4626Upgradeable, OwnableUpgradeable, PausableUpgradeable {
         view
         returns (uint256)
     {
-        //TODO: //return IdleCDOTranche(_tranche).totalSupply() // this should be used for the idle integration
+        //IERC20Upgradeable(IYieldSourceContract(yieldSourceVault).AATranche()).totalSupply(); //for idle integration
         return IYieldSourceContract(yieldSourceVault).getTotalValueLocked();
     }
 
